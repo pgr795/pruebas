967a5c3238fa8abd208c5af048b0da100bf1d4db
Pablo Garcia <pgr795@gmail.com>
2023-05-28 18:26:37 +0200

    GithubV2

--- v5.2/public_html/Sql/SQL_Gaming_Room_Asignar_Equipos.sql
+++
@@ -1,8 +0,0 @@
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 1;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 2;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 3;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 4;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 5;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 6;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 7;
-UPDATE equipo SET torneo = '4' WHERE equipo.idequipo = 8;
